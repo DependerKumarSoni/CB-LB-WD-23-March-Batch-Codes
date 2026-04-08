19a18827c4afc282aeb372c863b296054a7d19ac
DependerKumarSoni <dependerkumarsoni@gmail.com>
2026-04-09 00:21:12 +0530

    20260408 - Class 7 - JS continued.

---
+++ 20260408 - Class 7 - JS Continued/script.js
@@ -0,0 +1,96 @@
+// var s = sum(2, 3, 4, 5, 6);
+// console.log("Time: ", time);
+
+// function sum (a, b, c) {
+//     return a + b + c;
+// }
+
+// function sum (a, b) {
+//     return a * b;
+// }
+
+// function sum (num1, num2) {
+//     console.log(num1/num2);
+// }
+
+// var time = "17599585568";
+
+// console.log("Time2: ", time);
+
+
+// // interview question.
+// // rest operator.
+// function sum (a, c, ...b) {
+//     console.log(a, b, c);
+//     console.log("type of a", typeof a);
+//     console.log("type of b", typeof b);
+//     console.log("type of c", typeof c);
+// }
+
+// // spread operator
+// arr = ['abc', 'edf'];
+// arr2 = ['mpq', ...arr];
+
+// console.log(arr2);
+
+
+// these functions belong to a particul
+
+// function checkInput (input) {
+//     if (input == 0) {
+//         return;
+//     }
+//     if (input == 1) {
+//         console.log("Option 1 registered");
+//         print();
+//     }
+// }
+
+
+// function print() {
+//     console.log("Hi there...!");
+//     input = prompt("Option Please: ");
+//     checkInput(input);
+//     return;
+// }
+
+// print();
+
+// var p;
+
+// function calculate () {
+//     var a = 10;
+//     var b = 20;
+
+//     function add (a, b) {
+//         var c = 20;
+//         var d = 50;
+//         console.log( c + d);
+//     }
+    
+//     function sub (a, b) {
+//         console.log( a - b);
+//     }
+    
+//     add(a, b);
+//     sub(a, b);
+// }
+
+// calculate();
+
+// p = "Hey, Don't forget me..!";
+// console.log(p);
+
+
+
+// arr = ["Rohan","Dhruv","Aditya","Navneer"]
+// console.log(arr);
+// console.log(...arr);
+// console.log(typeof arr);
+// console.log(arr.length);
+
+
+// var arr = new Array(1, 2, 3);
+// console.log(arr);
+
+
